9b1d81e4af592da73569c4a85595f3eaa3927c54
Jason Berney <jasonrberney@gmail.com>
2018-03-07 15:50:49 -0800

    Dispatch actions to update state for a user.

--- src/containers/Login/LoginContainer.jsx
+++ src/containers/Login/LoginContainer.jsx
@@ -2,21 +2,27 @@ import React, { Component } from 'react'
 import Login from '../../components/Login/Login.jsx'
 import auth from '../../helpers/auth.jsx'
 import { connect } from 'react-redux'
+import { bindActionCreators, dispatch } from 'redux'
 import * as appUsersActionCreators from '../../redux/appUsers.jsx'
 
 class LoginContainer extends Component {
     constructor(props) {
         super(props)
+
+        this.handleAuth = this.handleAuth.bind(this)
     }
 
     handleAuth () {
+        this.props.dispatch(appUsersActionCreators.fetchingUser())
         auth().then((user) => {
+            this.props.dispatch(appUsersActionCreators.fetchingUserSuccess(user.uid, user, Date.now()))
+            this.props.dispatch(appUsersActionCreators.authUser(user.uid))
             console.log('Authed User', user)
         })
+        .catch((error) => this.props.dispatch(appUsersActionCreators.fetchingUserFailure(error)))
     }
 
     render() {
-        console.log(this.props)
         return (
             <Login isFetching={this.props.isFetching} error={this.props.error} onAuth={this.handleAuth} />
         )
@@ -24,10 +30,15 @@ class LoginContainer extends Component {
 }
 
 function mapStateToProps(state) {
+    console.log('STATE', state)
     return {
         isFetching: state.appUsers.isFetching,
         error: state.appUsers.error,
     }
+}
+
+function mapDispatchToProps (dispatch) {
+    return bindActionCreators(appUsersActionCreators, dispatch)
 }
 
 export default connect(mapStateToProps)(LoginContainer)
