1d5658b2c65735941be22124702deaf000fbcb87
Jason Berney <jasonrberney@gmail.com>
2018-03-09 16:48:07 -0800

    Add Redux dev tools.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 //import routes from './config/routes.jsx'
@@ -11,14 +11,18 @@ import appUsers from './redux/appUsers.jsx'
 import { checkIfAuthed } from './helpers/auth.jsx'
 
 const reducers = combineReducers({mapboxMapInfo, mapData, appUsers})
-export const store = createStore(reducers, applyMiddleware(thunk))
+export const store = createStore(reducers, compose(
+    applyMiddleware(thunk),
+    // hooks up Redux chrome extension. If extension does not exist, only take the first argument
+    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+))
 
 // For protected routes. Re-routes you to mapjournal if logged in
 function checkAuth(nextState, replace) {
-    if (store.getState().isFetching === true) {
+    if (store.getState().appUsers.isFetching === true) {
         return
     }
-    
+
     const isAuthed = checkIfAuthed(store)
     const nextPathName = nextState.location.pathname
     if (nextPathName === '/') {
